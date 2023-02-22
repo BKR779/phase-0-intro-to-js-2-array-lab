125d9fee331cdec20628ff8a609388252f705e3d
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-02-23 02:31:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+  cat = cats.push("Ralph")
+}
+
+function destructivelyPrependCat(cat) {
+  cat = cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(cat) {
+  cat = cats.pop()
+}
+function destructivelyRemoveFirstCat(cat) {
+  cat = cats.shift()
+}
+
+function appendCat(cat) {
+  cat = [...cats, "Broom"]
+  return cat;
+}
+
+function prependCat(cat) {
+  cat = [cat, ...cats]
+  return cat;
+}
+
+function removeLastCat(cat) {
+  cat = [...cats];
+  cat.pop();
+  return cat;
+}
+
+function removeFirstCat(cat) {
+  cat = [...cats];
+  cat.shift();
+  return cat;
+}
